2b02565420a43fea6083cd3046878fbff8ed6681
damian <damian@damian.id.au>
2017-10-07 16:17:55 +1100

    Updates!

---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+  /* install.php
+   * - homelabdash
+   * Damian West - damian@damian.id.au
+   * An update of /u/_SleepingBag_'s original homelabdash static script
+   * -
+   * Installation script for homelabdash
+   * -
+   * This script will configure the database for use with the homelabdash, create a
+   * default config.php, and then ask for the local servers / services on the network.
+   */
+  
+  // prompt for 
+?>
